cf1c652c1c525034223721361b5a33b1782d9cbe
shahrad <shahradArianNezhad@gmail.com>
2026-06-03 18:09:18 +0330

    fixed git problems

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include "./engine/engine.hpp"
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include "./game/game.hpp"
+#include "engine/entityManager/component/components.hpp"
+
+class myGame : public Game{
+public:
+  int counter=0;
+  bool face=0;
+  EntityId camera=engine.entityManager.makeCamera();
+  EntityId c2 =engine.makeCircle({0,0,0},150);
+  EntityId c3 =engine.makeCircle({-300,0,0},100);
+
+  void init() override {
+    auto rect = CircleColliderComponent{150,{0,0}};
+    engine.entityManager.componentManager.setComponent<ComponentType::CIRCLECOLLIDER>(c2,rect);
+    rect = CircleColliderComponent{100,{0,0}};
+    engine.entityManager.componentManager.setComponent<ComponentType::CIRCLECOLLIDER>(c3,rect);
+    auto transform = engine.entityManager.componentManager.getComponent<ComponentType::TRANSFORM>(c2);
+    engine.entityManager.componentManager.setComponent<ComponentType::TRANSFORM>(c2, transform);
+    auto cam = engine.entityManager.componentManager.getComponent<ComponentType::CAMERA2D>(camera);
+    engine.entityManager.componentManager.setComponent<ComponentType::CAMERA2D>(camera,cam);
+    engine.useCamera(camera, engine.getCurrentScene());
+    engine.setTargetFPS(60);
+
+  }
+
+
+  void update(double) override {
+    auto transform = engine.entityManager.componentManager.getComponent<ComponentType::TRANSFORM>(c2);
+    if(engine.inputHandler.checkKeyPress(Key::D))transform.position.x+=10;
+    if(engine.inputHandler.checkKeyPress(Key::A))transform.position.x-=10;
+    if(engine.inputHandler.checkKeyPress(Key::W)) transform.position.y-=10;
+    if(engine.inputHandler.checkKeyPress(Key::S)) transform.position.y+=10;
+
+
+
+    engine.entityManager.componentManager.setComponent<ComponentType::TRANSFORM>(c2, transform);
+    if(engine.isColliding(c2,c3)) LOG_WARN("COLLIDE");
+  };
+};
+
+int main() {
+  myGame game;
+  game.run();
+  return 0;
+}
